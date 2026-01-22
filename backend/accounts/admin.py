7eb9d749c3bb8f7e515291cf5afe882e21d1d490
vincent <vincegams24@gmail.com>
2026-01-21 22:57:37 -0800

    first commit

---
+++ backend/accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+@admin.register(CustomUser)
+class CustomAdminUser(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    model = CustomUser
