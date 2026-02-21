30bb157616a09163ec9cd050c916d9455cd083f4
jozuacydsagradojuariojcsj <jozuacydjuariojcsj@gmail.com>
2026-02-21 19:42:29 +0800

    Just changed the modal will continue tomorrow

--- frontend/src/components/dialog/PDFDialog.jsx
+++ frontend/src/components/dialog/PDFDialog.jsx
@@ -16,13 +16,33 @@ const PDFDialog = ({ data, onClose }) => {
 
   return (
     // <dialog className="modal modal-open">
-    //   <div className="modal-box  border border-black ">
-    //     <iframe src={`${data.lesson_plan}`} className="w-full h-full" />
+    //   <div className="modal-box w-11/12 max-w-5xl h-[90vh] gap-y-2 flex flex-col">
+    //     <div className="p-4 border-b flex justify-between">
+    //       <h2 className="font-semibold">Lesson Plan Preview</h2>
+    //       <button onClick={onClose}>✕</button>
+    //     </div>
+    //     <div className="flex flex-1">
+    //       <iframe src={`${data.lesson_plan}`} className="w-full h-full" />
+    //     </div>
+    //     <div className="w-full border border-black flex justify-center md:justify-evenly items-center">
+    //       <button
+    //         className="flex md:hidden border border-yellow-500 justify-center items-center w-3/4"
+    //         onClick={() => setOpenReview(true)}>
+    //         Review Lesson Plan
+    //       </button>
+    //       <button className="btn rounded-full w-40 h-10 btn-outline btn-success">
+    //         SAVE
+    //       </button>
+    //     </div>
     //   </div>
+
     //   <div className="modal-backdrop" onClick={onClose}></div>
+    //   {openReview && <ReviewDialog onClose={() => setOpenReview(false)} />}
     // </dialog>
 
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+    <div
+      className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+      onClick={onClose}>
       <div className="bg-white w-11/12 max-w-5xl h-[90vh] rounded-lg flex flex-col gap-y-2">
         {/* Header */}
         <div className="p-4 border-b flex justify-between">
@@ -38,7 +58,7 @@ const PDFDialog = ({ data, onClose }) => {
         <div className="border border-black w-full h-20">Feedback</div>
 
         <button
-          className="border border-green-500 rounded-full"
+          className="block border border-green-500 rounded-full md:hidden"
           onClick={() => setOpenReview(true)}>
           Review Lesson Plan
         </button>
--- frontend/src/components/dialog/ReviewDialog.jsx
+++ frontend/src/components/dialog/ReviewDialog.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const ReviewDialog = ({ onClose }) => {
   return (
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+    <div
+      className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+      onClick={onClose}>
       <div className="bg-white w-1/2 max-w-5xl h-[50vh] rounded-lg flex flex-col gap-y-2 p-2">
         {/* Header */}
         <div className="p-4 border-b flex justify-between">
